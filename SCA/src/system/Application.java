358b59879da6a1302024d034ee674f36fd04d8bb
Yoselin Rojas <79998813+JRojas89@users.noreply.github.com>
2021-11-05 10:01:29 -0600

    Nada importante

--- SCA/src/system/Application.java
+++ SCA/src/system/Application.java
@@ -12,11 +12,11 @@ public class Application {
 
     public static void main(String[] args) {
         Cliente clien1 = new Cliente();
-        //Cliente clien2 = new Cliente("Mari1970", "password", 20000);
+        Cliente clien3 = new Cliente("JM94", "password", 20000);
         
         try {
-            clien1 = Service.instance().clienteGet("Pedro89");
-            //Service.instance().clienteAdd(clien2);
+            clien1 = Service.instance().clienteGet("Mari1970");
+            //Service.instance().clienteAdd(clien3);
         } catch (Exception ex) {}
          
         System.out.println("Usuario: " + clien1.getUsuario() + "\nSaldo: " + clien1.getSaldoCuenta());
